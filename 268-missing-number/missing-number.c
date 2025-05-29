71f010d5939d715a06fcced44f54d256de0cee90
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-05-29 22:48:03 +0530

    Time: 1098 ms (5.02%) | Memory: 8.9 MB (65.64%) - LeetSync

---
+++ 268-missing-number/missing-number.c
@@ -0,0 +1,20 @@
+int missingNumber(int* nums, int numsSize) {
+    for(int i=0;i<numsSize;i++){
+        for(int j=i+1;j<numsSize;j++){
+            if(nums[i]>nums[j]){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+            }
+        }
+    }
+    if(nums[0]!=0){
+        return 0;
+    }
+    for(int i=1;i<numsSize;i++){
+        if((nums[i]-nums[i-1])>1){
+            return(nums[i-1]+1);
+        }
+    }
+    return numsSize;
+}
