8cf775e9a034d99e156cba6e06e0e8a94c23ef6c
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-05-29 18:12:13 +0530

    Time: 15 ms (64.04%) | Memory: 18.7 MB (47.99%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if(s.length()<=0){
+            return s;
+        }
+        // creating an empty string which stores the result string
+        string res="";
+        // we are applying Expand around center logic
+        // consider odd length
+        for(int i=0;i<s.length();i++){
+            int low=i;
+            int high=i;
+            // expanding to the left and right from center
+            while(low>=0 && high<s.length() && s[low]==s[high]){
+                low--;
+                high++;
+            }
+            // try to replace the longest palindrome substring
+            string palindrome=s.substr(low+1,high-low-1);
+            if(palindrome.length()>res.length()){
+                res=palindrome;
+            }
+            // consider even length
+            low=i-1;
+            high=i;
+            while(low>=0 && high<s.length() && s[low]==s[high]){
+                low--;
+                high++;
+            }
+            palindrome=s.substr(low+1,high-low-1);
+            if(palindrome.length()>res.length()){
+                res=palindrome;
+            }
+        }
+        return res;
+    }
+};
