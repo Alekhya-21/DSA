5e3a9de6f7822ed5a310322dd21a5b24348b44ca
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-05-31 15:49:43 +0530

    Time: 17 ms (95.38%) | Memory: 71 MB (85.14%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -8,46 +8,5 @@ public:
         }
     }
     return 0;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // sort(nums.begin(),nums.end());
-    // bool flag=false;
-    // for(int i=0;i<nums.size()-1;i++){
-    //     if(nums[i]==nums[i+1]){
-    //         flag=true;
-    //         break;
-    //     }
-    // }
-    // if(flag==true){
-    //     return true;
-    // }
-    // else{
-    //     return false;
-    // }
     }
 };
