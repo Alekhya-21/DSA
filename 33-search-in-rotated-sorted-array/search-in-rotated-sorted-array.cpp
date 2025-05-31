e80bc4b6bf0f7246644df8399d9d7a339e739dee
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-05-31 13:59:39 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (32.99%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -7,39 +7,5 @@ public:
             }
         }
         return -1;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // bool flag=false;
-        // for(int i=0;i<nums.size();i++){
-        //     if(nums[i]==target){
-        //         return i;
-        //     }
-        //     flag=true;
-        // }
-        // if(!flag){
-        //     return -1;
-        // }
-        // return -1;
     }
 };
