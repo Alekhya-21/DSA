d9c9c0acbffbdd5e612a79acfdd115ddbe5efeb2
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-05-31 15:11:03 +0530

    Time: 6 ms (8.30%) | Memory: 10.8 MB (6.18%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,59 +1,21 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
+    string s1;
     for(int i=0;i<s.length();i++){
-        s[i]=tolower(s[i]);
-    }
-    string str;
-    for(int i=0;i<s.length();i++){
-        if(isalpha(s[i]) || isdigit(s[i])){
-            str+=s[i];
+        if(isalpha(s[i])){
+            s[i]=tolower(s[i]);
+            s1.push_back(s[i]);
+        }
+        else if(isdigit(s[i])){
+            s1.push_back(s[i]);
         }
     }
-    string res=str;
-    reverse(str.begin(),str.end());
-    if(res==str){
+    string s2=s1;
+    reverse(s1.begin(),s1.end());
+    if(s1==s2){
         return 1;
     }
-    return 0;  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // string s1;
-    // for(int i=0;i<s.length();i++){
-    //     if(isalpha(s[i])){
-    //         s[i]=tolower(s[i]);
-    //         s1.push_back(s[i]);
-    //     }
-    //     else if(isdigit(s[i])){
-    //         s1.push_back(s[i]);
-    //     }
-    // }
-    // string s2=s1;
-    // reverse(s1.begin(),s1.end());
-    // if(s1==s2){
-    //     return 1;
-    // }
-    // return 0;
+    return 0;
     }
 };
