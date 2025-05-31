24c0460101c808be9acc849d4839fde8a149fe8e
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-05-31 14:25:09 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (17.13%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -19,54 +19,5 @@ public:
         res.push_back(-1);
     }
     return res;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // vector<int> neww(2);
-    // int count=0;
-    // for(int i=0;i<nums.size();i++){
-    //     if(nums[i]==target){
-    //         count++;
-    //         neww[0]=i;
-    //         break;
-    //     }
-    // }
-    // for(int i=nums.size()-1;i>=0;i--){
-    //     if(nums[i]==target){
-    //         count++;
-    //         neww[1]=i;
-    //         break;
-    //     }
-    // }
-    // if(count==0 || nums.size()==0){
-    //     neww[0]=-1;
-    //     neww[1]=-1;
-    //     return neww;
-    // }
-    // return neww;
     }
 };
