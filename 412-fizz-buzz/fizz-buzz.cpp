5318a2e4ed4cce5e6c0dad8187bf52afae20bd40
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-06-02 18:10:00 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (43.60%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.cpp
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -17,57 +17,5 @@ public:
         }
     }
     return ll;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // vector<string> str(n);
-        // for(int i=1;i<=n;i++){
-        //     if(i%3==0 && i%5==0){
-        //         str[i-1]="FizzBuzz";
-        //     }
-        //     else if(i%3==0){
-        //         str[i-1]="Fizz";
-        //     }
-        //     else if(i%5==0){
-        //         str[i-1]="Buzz";
-        //     }
-        //     else{
-        //         str[i-1]=to_string(i);
-        //     }
-        // }
-        // return str;
     }
 };
