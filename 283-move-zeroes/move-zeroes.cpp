cab0af2bfc1c6e0d7c52a70e1ada6bc712433d8b
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-06-02 16:49:32 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (78.20%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,40 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-    int low=0,high=1;
+    int low=0,high=0;
     while(high<nums.size()){
-        if(nums[high]!=0 && nums[low]==0){
+        if(nums[high]!=0){
             swap(nums[low],nums[high]);
-            low++;
-        }
-        if(nums[low]!=0){
             low++;
         }
         high++;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // int low=0,high=0;
-    // while(high<nums.size()){
-    //     if(nums[high]!=0){
-    //         swap(nums[low],nums[high]);
-    //         low++;
-    //     }
-    //     high++;
-    // }
     }
 };
