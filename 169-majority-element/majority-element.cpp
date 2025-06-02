e3ca71a7bc1b52702a2f2c8606add43ffaae9f1e
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-06-02 16:57:28 +0530

    Time: 2395 ms (5.02%) | Memory: 28.3 MB (24.01%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,22 +1,55 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-    if(nums.size()<2){
-        return nums[0];
-    }
-    int count=1;
-    sort(nums.begin(),nums.end());
-    for(int i=0;i<nums.size()-1;i++){
-        if(nums[i]==nums[i+1]){
-            count++;
-            if(count>(nums.size()/2)){
-                return nums[i];
-            }
-        }
-        else{
-            count=1;
+    for(int i=0;i<nums.size();i++){
+        int c=count(nums.begin(),nums.end(),nums[i]);
+        if(c>nums.size()/2){
+            return nums[i];
         }
     }
-    return 0;
+    return -1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // if(nums.size()<2){
+    //     return nums[0];
+    // }
+    // int count=1;
+    // sort(nums.begin(),nums.end());
+    // for(int i=0;i<nums.size()-1;i++){
+    //     if(nums[i]==nums[i+1]){
+    //         count++;
+    //         if(count>(nums.size()/2)){
+    //             return nums[i];
+    //         }
+    //     }
+    //     else{
+    //         count=1;
+    //     }
+    // }
+    // return 0;
     }
 };
