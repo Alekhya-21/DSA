cef2f4d715a7264fff73b443f80a6802a8a7432d
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-06-02 18:09:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (15.55%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.cpp
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -1,21 +1,73 @@
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-        vector<string> str(n);
-        for(int i=1;i<=n;i++){
-            if(i%3==0 && i%5==0){
-                str[i-1]="FizzBuzz";
-            }
-            else if(i%3==0){
-                str[i-1]="Fizz";
-            }
-            else if(i%5==0){
-                str[i-1]="Buzz";
-            }
-            else{
-                str[i-1]=to_string(i);
-            }
+    vector<string> ll;
+    for(int i=0;i<n;i++){
+        if((i+1)%3==0 && (i+1)%5==0){
+            ll.push_back("FizzBuzz");
         }
-        return str;
+        else if((i+1)%5==0){
+            ll.push_back("Buzz");
+        }
+        else if((i+1)%3==0){
+            ll.push_back("Fizz");
+        }
+        else{
+            ll.push_back(to_string(i+1));
+        }
+    }
+    return ll;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // vector<string> str(n);
+        // for(int i=1;i<=n;i++){
+        //     if(i%3==0 && i%5==0){
+        //         str[i-1]="FizzBuzz";
+        //     }
+        //     else if(i%3==0){
+        //         str[i-1]="Fizz";
+        //     }
+        //     else if(i%5==0){
+        //         str[i-1]="Buzz";
+        //     }
+        //     else{
+        //         str[i-1]=to_string(i);
+        //     }
+        // }
+        // return str;
     }
 };
