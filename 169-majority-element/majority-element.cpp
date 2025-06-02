f296cf38c2627cc20f5ee5d9ae0d1dedce381cbb
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-06-02 16:57:49 +0530

    Time: 2426 ms (5.02%) | Memory: 28.1 MB (84.11%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -8,48 +8,5 @@ public:
         }
     }
     return -1;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // if(nums.size()<2){
-    //     return nums[0];
-    // }
-    // int count=1;
-    // sort(nums.begin(),nums.end());
-    // for(int i=0;i<nums.size()-1;i++){
-    //     if(nums[i]==nums[i+1]){
-    //         count++;
-    //         if(count>(nums.size()/2)){
-    //             return nums[i];
-    //         }
-    //     }
-    //     else{
-    //         count=1;
-    //     }
-    // }
-    // return 0;
     }
 };
