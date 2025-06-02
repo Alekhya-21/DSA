5bc56aa1091d2f2ea92122e16785631a18be3424
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-06-02 17:25:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (45.63%) - LeetSync

--- 263-ugly-number/ugly-number.cpp
+++ 263-ugly-number/ugly-number.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        if(n==0){
-            return 0;
-        }
+    if(n==0){
+        return 0;
+    }
     while(n%5==0){
         n=n/5;
     }
