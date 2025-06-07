5cb5df91548766a3b6b451c5f29adfc568c4c69a
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-06-07 22:50:59 +0530

    Time: 1 ms (53.90%) | Memory: 11.6 MB (23.22%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,29 +1,73 @@
 class Solution {
 public:
     string reverseWords(string s) {
-    vector<string> ll;
-    string temp;
-    // iterating through the string, a temporary string created
-    // appending the temp to list if a space is encountered
-    for(int i=0;i<s.length();i++){
+    vector<string> arr;
+    string str;
+    for(int i=0;i<s.size();i++){
         if(s[i]!=' '){
-            temp+=s[i];
+            str+=s[i];
         }
-        if(s[i]==' ' && temp.size()>0){
-            ll.push_back(temp);
-            temp="";
+        if(s[i]==' ' && str.size()>0){
+            arr.push_back(str);
+            str="";
         }
     }
-    if(temp.length()>0){
-        ll.push_back(temp);
+    if(str.size()>0){
+        arr.push_back(str);
     }
-    reverse(ll.begin(),ll.end());
+    reverse(arr.begin(),arr.end());
     string res;
-    for(string i:ll){
+    for(string i:arr){
         res+=i;
         res+=' ';
     }
-    string result=res.substr(0,res.length()-1);
-    return result;
+    res=res.substr(0,res.length()-1);
+    return res;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // vector<string> ll;
+    // string temp;
+    // // iterating through the string, a temporary string created
+    // // appending the temp to list if a space is encountered
+    // for(int i=0;i<s.length();i++){
+    //     if(s[i]!=' '){
+    //         temp+=s[i];
+    //     }
+    //     if(s[i]==' ' && temp.size()>0){
+    //         ll.push_back(temp);
+    //         temp="";
+    //     }
+    // }
+    // if(temp.length()>0){
+    //     ll.push_back(temp);
+    // }
+    // reverse(ll.begin(),ll.end());
+    // string res;
+    // for(string i:ll){
+    //     res+=i;
+    //     res+=' ';
+    // }
+    // string result=res.substr(0,res.length()-1);
+    // return result;
     }
 };
