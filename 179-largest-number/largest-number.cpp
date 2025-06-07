bfc0737929f61c229077340f0456daf531b51d98
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-06-07 22:20:38 +0530

    Time: 7 ms (27.54%) | Memory: 16.3 MB (76.30%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -20,50 +20,5 @@ public:
         str+=to_string(i);
     }
     return str;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-        // bool flag=true;
-        // for(int i=0;i<nums.size();i++){
-        //     if(nums[i]!=0){
-        //         flag=false;
-        //     }
-        // }
-        // if (flag) return "0";
-        // sort(nums.begin(),nums.end(),[](int a,int b){
-        //     string s1=to_string(a)+to_string(b);
-        //     string s2=to_string(b)+to_string(a);
-        //     return s1>s2;
-        // });
-        // string str;
-        // for(int i:nums){
-        //     str+=to_string(i);
-        // }
-        // return str;
     }
 };
