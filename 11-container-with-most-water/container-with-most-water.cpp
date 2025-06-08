a457e1a03d4e5eb7ded9b814a7e8d24e7496e8ef
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-06-08 15:32:43 +0530

    Time: 1 ms (45.88%) | Memory: 63.1 MB (12.48%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,21 +1,57 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-    int maxAreaHehe=0;
-    int area=0;
-    int low=0,high=height.size()-1;
-    while(low<high){
-        int heightt=min(height[low],height[high]);
-        int width=high-low;
-        area=heightt*width;
-        maxAreaHehe=max(area,maxAreaHehe);
-        if(height[low]<height[high]){
-            low++;
+        int maxArea=0;
+        int vertical=0;
+        int area=0;
+        int low=0,high=height.size()-1;
+        while(low<high){
+            vertical=min(height[low],height[high]);
+            area=vertical*(high-low);
+            if(area>maxArea){
+                maxArea=area;
+            }
+            if(height[low]<height[high]){
+                low++;
+            }
+            else{
+                high--;
+            }
         }
-        else{
-            high--;
-        }
-    }
-    return maxAreaHehe;
+        return maxArea;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // int maxAreaHehe=0;
+    // int area=0;
+    // int low=0,high=height.size()-1;
+    // while(low<high){
+    //     int heightt=min(height[low],height[high]);
+    //     int width=high-low;
+    //     area=heightt*width;
+    //     maxAreaHehe=max(area,maxAreaHehe);
+    //     if(height[low]<height[high]){
+    //         low++;
+    //     }
+    //     else{
+    //         high--;
+    //     }
+    // }
+    // return maxAreaHehe;
     }
 };
