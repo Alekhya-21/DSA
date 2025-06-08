7893a8156a3c346fb8bc34759ac5b504d22bab5d
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-06-08 17:45:26 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (67.21%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -4,12 +4,11 @@ public:
     int i=0;
     string s;
     string res;
-    string first=strs[0];
-    while(i<first.length()){
-        s+=first[i];
+    while(i<strs[0].length()){
+        s+=strs[0][i];
         int count=0;
-        for(int i=0;i<strs.size();i++){
-            if(strs[i].find(s)==0){
+        for(int j=0;j<strs.size();j++){
+            if(strs[j].find(s)==0){
                 count++;
             }
         }
@@ -20,7 +19,49 @@ public:
             return res;
         }
         i++;
-    }   
+    }
     return res;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // int i=0;
+    // string s;
+    // string res;
+    // string first=strs[0];
+    // while(i<first.length()){
+    //     s+=first[i];
+    //     int count=0;
+    //     for(int i=0;i<strs.size();i++){
+    //         if(strs[i].find(s)==0){
+    //             count++;
+    //         }
+    //     }
+    //     if(count==strs.size()){
+    //         res=s;
+    //     }
+    //     else{
+    //         return res;
+    //     }
+    //     i++;
+    // }   
+    // return res;
     }
 };
