9ea515ccd29b83b3e6860c18811d58dd09be598d
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-06-10 13:42:57 +0530

    Time: 0 ms (100.00%) | Memory: 32.1 MB (39.39%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,64 +1,27 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-    int low=0;
-    int high=0;
+    int n=nums.size();
     int minWindow=INT_MAX;
-    int sum=0;
-    bool flag=false;
-    while(high<nums.size()){
-        sum+=nums[high];
+    int flag=0;
+    int currSum=0;
+    int low=0,high=0; // two pointers
+    while(high<n){
+        currSum+=nums[high];
         high++;
-        while(sum>=target){
-            int currWindow=high-low;
-            minWindow=min(minWindow,currWindow);
-            flag=true;
-            sum-=nums[low];
+        while(currSum>=target){
+            int currWindowSize=high-low;
+            minWindow=min(minWindow,currWindowSize);
+            flag=1;
+            currSum-=nums[low];
             low++;
         }
     }
-    if(flag==true){
+    if(flag==1){
         return minWindow;
     }
-    return 0;     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // int n=nums.size();
-    // int minWindow=INT_MAX;
-    // int flag=0;
-    // int currSum=0;
-    // int low=0,high=0; // two pointers
-    // while(high<n){
-    //     currSum+=nums[high];
-    //     high++;
-    //     while(currSum>=target){
-    //         int currWindowSize=high-low;
-    //         minWindow=min(minWindow,currWindowSize);
-    //         flag=1;
-    //         currSum-=nums[low];
-    //         low++;
-    //     }
-    // }
-    // if(flag==1){
-    //     return minWindow;
-    // }
-    // else{
-    //     return 0;
-    // }
+    else{
+        return 0;
+    }
     }
 };
