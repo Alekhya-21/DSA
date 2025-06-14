e3117d77344af38503f246110df02cca89468c69
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-06-14 14:19:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (96.47%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        string str;
-        for (int i = 0; i < s.length(); i++) {
-            if (!str.empty() && 
-                ((s[i] == ')' && str.back() == '(') ||
-                 (s[i] == ']' && str.back() == '[') ||
-                 (s[i] == '}' && str.back() == '{'))) {
-                str.pop_back();
-            } else {
-                str.push_back(s[i]);
-            }
+    bool isValid(string s) {    string str;
+    for(int i=0;i<s.length();i++){
+        if(!str.empty() && 
+            ((s[i]==')' && str.back()=='(') ||
+            (s[i]=='}' && str.back()=='{') ||
+            (s[i]==']' && str.back()=='['))){
+            str.pop_back();
         }
-        return str.empty();
+        else{
+            str.push_back(s[i]);
+        }
+    }  
+    return str.empty();
     }
 };
