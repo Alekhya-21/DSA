71dfda9e4a725c09075cf9e056ebee3ef7237ea6
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-07-12 17:51:17 +0530

    Time: 6 ms (32.43%) | Memory: 16.3 MB (77.24%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -1,24 +1,63 @@
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
-    bool flag=false;
-    for(int i=0;i<nums.size();i++){
-        if(nums[i]!=0){
-            flag=true;
+        bool flag=true;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                flag=false;
+            }
         }
-    }
-    if(flag==false){
-        return "0";
-    }
+        if(flag) return "0";
     sort(nums.begin(),nums.end(),[](int a,int b){
-    string s1=to_string(a)+to_string(b);
-    string s2=to_string(b)+to_string(a);
-    return s1>s2;
+        string s1=to_string(a)+to_string(b);
+        string s2=to_string(b)+to_string(a);
+        return s1>s2;
     });
-    string str;
+    string res;
     for(int i:nums){
-        str+=to_string(i);
+        res+=to_string(i);
     }
-    return str;
+    return res;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // bool flag=false;
+    // for(int i=0;i<nums.size();i++){
+    //     if(nums[i]!=0){
+    //         flag=true;
+    //     }
+    // }
+    // if(flag==false){
+    //     return "0";
+    // }
+    // sort(nums.begin(),nums.end(),[](int a,int b){
+    // string s1=to_string(a)+to_string(b);
+    // string s2=to_string(b)+to_string(a);
+    // return s1>s2;
+    // });
+    // string str;
+    // for(int i:nums){
+    //     str+=to_string(i);
+    // }
+    // return str;
     }
 };
