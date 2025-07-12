f38545dccaad00f27ac8e7d07db692a88f52d273
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-07-12 15:13:39 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (61.03%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -1,10 +1,10 @@
 class NumArray {
 public:
-    vector<int> arr;
+vector<int> arr;
     NumArray(vector<int>& nums) {
         arr=nums;
         for(int i=1;i<nums.size();i++){
-            arr[i]=arr[i-1]+arr[i];
+            arr[i]=arr[i]+arr[i-1];
         }
     }
     
@@ -12,7 +12,9 @@ public:
         if(left==0){
             return arr[right];
         }
-        return arr[right]-arr[left-1];
+        else{
+            return arr[right]-arr[left-1];
+        }
     }
 };
 
