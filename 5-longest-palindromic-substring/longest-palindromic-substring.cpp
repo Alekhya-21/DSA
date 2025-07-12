eb6f801357a56193a214ee4e03f19dba7a79ee73
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-07-12 20:56:16 +0530

    Time: 15 ms (60.99%) | Memory: 18.8 MB (45.49%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,87 +1,38 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-    string palindrome="";
-    for(int i=0;i<s.length();i++){
-        int low=i;
-        int high=i;
-        while(low>=0 && high<s.length() && s[low]==s[high]){
-            low--;
-            high++;
+        if(s.length()<=0){
+            return s;
         }
-        string str=s.substr(low+1,high-low-1);
-        if(str.length()>palindrome.length()){
-            palindrome=str;
+        // creating an empty string which stores the result string
+        string res="";
+        // we are applying Expand around center logic
+        // consider odd length
+        for(int i=0;i<s.length();i++){
+            int low=i;
+            int high=i;
+            // expanding to the left and right from center
+            while(low>=0 && high<s.length() && s[low]==s[high]){
+                low--;
+                high++;
+            }
+            // try to replace the longest palindrome substring
+            string palindrome=s.substr(low+1,high-low-1);
+            if(palindrome.length()>res.length()){
+                res=palindrome;
+            }
+            // consider even length
+            low=i-1;
+            high=i;
+            while(low>=0 && high<s.length() && s[low]==s[high]){
+                low--;
+                high++;
+            }
+            palindrome=s.substr(low+1,high-low-1);
+            if(palindrome.length()>res.length()){
+                res=palindrome;
+            }
         }
-        low=i-1;
-        high=i;
-        while(low>=0 && high<s.length() && s[low]==s[high]){
-            low--;
-            high++;
-        }
-        str=s.substr(low+1,high-low-1);
-        if(str.length()>palindrome.length()){
-            palindrome=str;
-        }
-    }
-    return palindrome;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // if(s.length()<=0){
-        //     return s;
-        // }
-        // // creating an empty string which stores the result string
-        // string res="";
-        // // we are applying Expand around center logic
-        // // consider odd length
-        // for(int i=0;i<s.length();i++){
-        //     int low=i;
-        //     int high=i;
-        //     // expanding to the left and right from center
-        //     while(low>=0 && high<s.length() && s[low]==s[high]){
-        //         low--;
-        //         high++;
-        //     }
-        //     // try to replace the longest palindrome substring
-        //     string palindrome=s.substr(low+1,high-low-1);
-        //     if(palindrome.length()>res.length()){
-        //         res=palindrome;
-        //     }
-        //     // consider even length
-        //     low=i-1;
-        //     high=i;
-        //     while(low>=0 && high<s.length() && s[low]==s[high]){
-        //         low--;
-        //         high++;
-        //     }
-        //     palindrome=s.substr(low+1,high-low-1);
-        //     if(palindrome.length()>res.length()){
-        //         res=palindrome;
-        //     }
-        // }
-        // return res;
+        return res;
     }
 };
