4aef1b2081f9d387ea9c47874886f5d8b0a7d340
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-07-14 20:59:47 +0530

    Time: 14 ms (19.65%) | Memory: 11 MB (40.30%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,40 +1,23 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-    int low=0,high=0;
+    int low=0;
+    int high=0;
     int maxWindow=0;
-    int windowSize=0;
     int maxFreq=0;
-    unordered_map<char,int> m1;
+    int windowSize=0;
+    unordered_map<char,int> count;
     while(high<s.length()){
-        m1[s[high]]++;
+        count[s[high]]++;
+        maxFreq=max(maxFreq,count[s[high]]);
         windowSize=high-low+1;
-        maxFreq=max(maxFreq,m1[s[high]]);
         if(windowSize-maxFreq>k){
-            m1[s[low]]--;
+            count[s[low]]--;
             low++;
         }
         maxWindow=max(maxWindow,high-low+1);
         high++;
     }
     return maxWindow;
-    // int low=0;
-    // int high=0;
-    // int maxWindow=0;
-    // int maxFreq=0;
-    // int windowSize=0;
-    // unordered_map<char,int> count;
-    // while(high<s.length()){
-    //     count[s[high]]++;
-    //     maxFreq=max(maxFreq,count[s[high]]);
-    //     windowSize=high-low+1;
-    //     if(windowSize-maxFreq>k){
-    //         count[s[low]]--;
-    //         low++;
-    //     }
-    //     maxWindow=max(maxWindow,high-low+1);
-    //     high++;
-    // }
-    // return maxWindow;
     }
 };
