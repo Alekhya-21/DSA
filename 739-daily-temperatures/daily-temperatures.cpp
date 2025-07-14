49ad90739c89a755415fd3e763f255734e9b0edd
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-07-14 22:56:04 +0530

    Time: 36 ms (15.24%) | Memory: 102.7 MB (91.57%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -4,69 +4,27 @@ public:
     vector<int> res(temperatures.size());
     stack<pair<int,int>> st;
     for(int i=temperatures.size()-1;i>=0;i--){
+        bool flag=false;
         if(i==temperatures.size()-1){
             res[i]=0;
-            st.push({temperatures[i],i});
         }
-        if(st.top().first>temperatures[i]){
+        else if(!st.empty() && st.top().first>temperatures[i]){
             res[i]=st.top().second-i;
-            st.push({temperatures[i],i});
         }
         else{
             while(!st.empty() && st.top().first<=temperatures[i]){
                 st.pop();
             }
-            if(!st.empty()){
+            if(st.empty()){
+                res[i]=0;
+                flag=true;
+            }
+            if(!flag){
                 res[i]=st.top().second-i;
             }
-            else{
-                res[i]=0;
-            }
-            st.push({temperatures[i],i});
         }
+        st.push({temperatures[i],i});
     }
     return res;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // vector<int> res(temperatures.size());
-    // stack<pair<int,int>> st;
-    // for(int i=temperatures.size()-1;i>=0;i--){
-    //     bool flag=false;
-    //     if(i==temperatures.size()-1){
-    //         res[i]=0;
-    //     }
-    //     else if(!st.empty() && st.top().first>temperatures[i]){
-    //         res[i]=st.top().second-i;
-    //     }
-    //     else{
-    //         while(!st.empty() && st.top().first<=temperatures[i]){
-    //             st.pop();
-    //         }
-    //         if(st.empty()){
-    //             res[i]=0;
-    //             flag=true;
-    //         }
-    //         if(!flag){
-    //             res[i]=st.top().second-i;
-    //         }
-    //     }
-    //     st.push({temperatures[i],i});
-    // }
-    // return res;
     }
 };
