f498baa646b992a299a58f992e31ecc94195ebe0
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-07-15 19:19:59 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (85.64%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -26,54 +26,6 @@ public:
             }
         }
     }
-    return st.top();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // vector<int> arr;
-    // for(int i=0;i<tokens.size();i++){
-    //     if(isdigit(tokens[i][0]) || (tokens[i][0]=='-' && tokens[i].length()>1)){
-    //         arr.push_back(stoi(tokens[i]));
-    //     }
-    //     else{
-    //         int b=arr[arr.size()-1];
-    //         arr.pop_back();
-    //         int a=arr[arr.size()-1];
-    //         arr.pop_back();
-    //         int temp;
-    //         if(tokens[i][0]=='+'){
-    //             temp=a+b;
-    //         }
-    //         else if(tokens[i][0]=='-'){
-    //             temp=a-b;
-    //         }
-    //         else if(tokens[i][0]=='*'){
-    //             temp=a*b;
-    //         }
-    //         else{
-    //             temp=a/b;
-    //         }
-    //         arr.push_back(temp);
-    //     }
-    // }
-    // return arr[arr.size()-1];   
+    return st.top();  
     }
 };
