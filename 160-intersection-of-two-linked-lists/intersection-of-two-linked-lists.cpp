6eaab1f4e0c4ed676fc62ea3177b47865b838d34
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-09-13 14:31:13 +0530

    Time: 36 ms (65.70%) | Memory: 18.5 MB (82.24%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,30 +9,87 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int lenA=1;
-        int lenB=1;
-        ListNode *tempA=headA;
-        ListNode *tempB=headB;
-        while(tempA->next!=NULL){
-            lenA++;
-            tempA=tempA->next;
-        }
-        while(tempB->next!=NULL){
-            lenB++;
-            tempB=tempB->next;
-        }
-        while(lenA>lenB){
-            lenA--;
-            headA=headA->next;
-        }
-        while(lenB>lenA){
-            lenB--;
-            headB=headB->next;
-        }
-        while(headA!=headB){
-            headA=headA->next;
-            headB=headB->next;
-        }
-        return headA;
+    int lenA=0;
+    int lenB=0;
+    ListNode* t1=headA;
+    ListNode* t2=headB;
+    while(t1!=NULL){
+        t1=t1->next;
+        lenA++;
+    }
+    while(t2!=NULL){
+        t2=t2->next;
+        lenB++;
+    }
+    while(lenA>lenB){
+        lenA--;
+        headA=headA->next;
+    }
+    while(lenB>lenA){
+        lenB--;
+        headB=headB->next;
+    }
+    while(headA!=headB){
+        headA=headA->next;
+        headB=headB->next;
+    }
+    return headA;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int lenA=1;
+        // int lenB=1;
+        // ListNode *tempA=headA;
+        // ListNode *tempB=headB;
+        // while(tempA->next!=NULL){
+        //     lenA++;
+        //     tempA=tempA->next;
+        // }
+        // while(tempB->next!=NULL){
+        //     lenB++;
+        //     tempB=tempB->next;
+        // }
+        // while(lenA>lenB){
+        //     lenA--;
+        //     headA=headA->next;
+        // }
+        // while(lenB>lenA){
+        //     lenB--;
+        //     headB=headB->next;
+        // }
+        // while(headA!=headB){
+        //     headA=headA->next;
+        //     headB=headB->next;
+        // }
+        // return headA;
     }
 };
