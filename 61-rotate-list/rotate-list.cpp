bb3edbc449c3ebe9f502a3203b3e5b7ce6c96f81
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-09-15 19:26:50 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (30.82%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -9,11 +9,11 @@
  * };
  */
 class Solution {
-ListNode* findlastnode(ListNode* head,int k){
+ListNode* lastnode(ListNode* head, int n){
     int count=1;
     ListNode* temp=head;
     while(temp!=NULL){
-        if(count==k){
+        if(count==n){
             return temp;
         }
         count++;
@@ -23,23 +23,23 @@ ListNode* findlastnode(ListNode* head,int k){
 }
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head==NULL || head->next==NULL){
-            return head;
-        }
-        ListNode* tail=head;
-        int len=1;
-        while(tail->next!=NULL){
-            len++;
-            tail=tail->next;
-        }
-        if(head==NULL || head->next==NULL || k%len==0 || k==0){
-            return head;
-        }
-        k=k%len;
-        tail->next=head;
-        ListNode* newlastnode=findlastnode(head,len-k);
-        head=newlastnode->next;
-        newlastnode->next=NULL;
+    int len=1;
+    if(head==NULL || head->next==NULL){
         return head;
+    }
+    ListNode* temp=head;
+    while(temp->next!=NULL){
+        len++;
+        temp=temp->next;
+    }
+    if(k==0 || k%len==0){
+        return head;
+    }
+    k=k%len;
+    temp->next=head;
+    ListNode* tail=lastnode(head,len-k);
+    head=tail->next;
+    tail->next=NULL;
+    return head;
     }
 };
