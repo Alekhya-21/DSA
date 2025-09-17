866bee96f953463379d1b2d08ac21ff37815af46
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-09-17 19:02:14 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (36.05%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,61 +1,24 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    vector<pair<int,int>> arr;
-    for(int i=0;i<nums.size();i++){
-        arr.push_back({nums[i],i});
-    }
-    sort(arr.begin(),arr.end());
-    int low=0,high=arr.size()-1;
-    while(low<high){
-        int sum=arr[low].first+arr[high].first;
-        if(sum==target){
-            return {arr[low].second,arr[high].second};
+        vector<pair<int,int>> arr;
+        for(int i=0;i<nums.size();i++){
+            arr.push_back({nums[i],i});
         }
-        else if(sum>target){
-            high--;
+        sort(arr.begin(),arr.end());
+        int low=0,high=nums.size()-1;
+        while(low<high){
+            int sum=arr[low].first+arr[high].first;
+            if(sum==target){
+                return {arr[low].second,arr[high].second};
+            }
+            else if(sum>target){
+                high--;
+            }
+            else{
+                low++;
+            }
         }
-        else{
-            low++;
-        }
-    }
-    return {};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // vector<pair<int,int>> arr;
-        // for(int i=0;i<nums.size();i++){
-        //     arr.push_back({nums[i],i});
-        // }
-        // sort(arr.begin(),arr.end());
-        // int low=0,high=nums.size()-1;
-        // while(low<high){
-        //     int sum=arr[low].first+arr[high].first;
-        //     if(sum==target){
-        //         return {arr[low].second,arr[high].second};
-        //     }
-        //     else if(sum>target){
-        //         high--;
-        //     }
-        //     else{
-        //         low++;
-        //     }
-        // }
-        // return {};
+        return {};
     }
 };
