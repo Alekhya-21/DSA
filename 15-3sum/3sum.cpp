0e2c7f27f6f619cc8e2c9d80fef4e24c192471f9
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-09-17 21:13:21 +0530

    Time: 1222 ms (8.57%) | Memory: 291.8 MB (9.31%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-    if(nums.size()<3){
-        return {};
-    }
     set<vector<int>> arr;
     sort(nums.begin(),nums.end());
-    for(int i=0;i<nums.size()-2;i++){
+    for(int i=0;i<=nums.size()-3;i++){
         int low=i+1;
         int high=nums.size()-1;
-        int sum=0;
         while(low<high){
-            sum=nums[i]+nums[low]+nums[high];
+            int sum=nums[i]+nums[low]+nums[high];
             if(sum==0){
                 arr.insert({nums[i],nums[low],nums[high]});
                 low++;
@@ -26,5 +22,54 @@ public:
         }
     }
     return vector<vector<int>>(arr.begin(),arr.end());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // if(nums.size()<3){
+    //     return {};
+    // }
+    // set<vector<int>> arr;
+    // sort(nums.begin(),nums.end());
+    // for(int i=0;i<nums.size()-2;i++){
+    //     int low=i+1;
+    //     int high=nums.size()-1;
+    //     int sum=0;
+    //     while(low<high){
+    //         sum=nums[i]+nums[low]+nums[high];
+    //         if(sum==0){
+    //             arr.insert({nums[i],nums[low],nums[high]});
+    //             low++;
+    //             high--;
+    //         }
+    //         else if(sum<0){
+    //             low++;
+    //         }
+    //         else{
+    //             high--;
+    //         }
+    //     }
+    // }
+    // return vector<vector<int>>(arr.begin(),arr.end());
     }
 };
