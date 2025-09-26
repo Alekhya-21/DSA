6a826f94041b5e861139d12fd23b46985ff04a6b
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-09-26 15:24:22 +0530

    Time: 0 ms (100.00%) | Memory: 112.4 MB (48.37%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        // eliminate if the total gas is lesser than the total cost it can never achieve
-        int total_gas=accumulate(gas.begin(),gas.end(),0);
-        int total_cost=accumulate(cost.begin(),cost.end(),0);
-        if(total_gas<total_cost){
-            return -1;
+    int total_gas=0,total_cost=0;
+    for(int i=0;i<gas.size();i++){
+        total_gas+=gas[i];
+        total_cost+=cost[i];
+    }
+    if(total_gas<total_cost){
+        return -1;
+    }
+    int total=0;
+    int starting_index=0;
+    for(int i=0;i<gas.size();i++){
+        total+=(gas[i]-cost[i]);
+        if(total<0){
+            starting_index=i+1;
+            total=0;
         }
-        int start_index=0;
-        int total=0;
-        for(int i=0;i<gas.size();i++){
-            total+=gas[i]-cost[i];
-            // if the value is negative, simply discard the index and move the 
-            // start index and reset the value
-            if(total<0){
-                start_index=i+1;
-                total=0;
-            }
-        }
-        return start_index;
+    }
+    return starting_index;
     }
 };
