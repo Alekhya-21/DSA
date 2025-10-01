6f270df573fe3fbbf16372c3ab030019dc4d1507
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-10-01 09:18:57 +0530

    Time: 6 ms (47.99%) | Memory: 11.1 MB (7.89%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-    int low=0;
-    int high=0;
-    int maxWindow=0;
-    int maxFreq=0;
-    int windowSize=0;
-    unordered_map<char,int> count;
-    while(high<s.length()){
-        count[s[high]]++;
-        maxFreq=max(maxFreq,count[s[high]]);
-        windowSize=high-low+1;
-        if(windowSize-maxFreq>k){
-            count[s[low]]--;
-            low++;
+        int low=0,high=0;
+        int max_freq=0;
+        int window_size=0;
+        int max_window=0;
+        unordered_map<char,int> count;
+        while(high<s.size()){
+            count[s[high]]++;
+            window_size=high-low+1;
+            max_freq=max(max_freq,count[s[high]]);
+            if((window_size-max_freq)>k){
+                count[s[low]]--;
+                low++;
+            }
+            max_window=max(max_window,high-low+1);
+            high++;
         }
-        maxWindow=max(maxWindow,high-low+1);
-        high++;
-    }
-    return maxWindow;
+        return max_window;
     }
 };
