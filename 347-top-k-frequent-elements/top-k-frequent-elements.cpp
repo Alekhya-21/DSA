0e39b8a9ca278262bf38a138ebdc9d0aed71f6ea
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-10-03 11:20:24 +0530

    Time: 3 ms (49.39%) | Memory: 18.2 MB (25.49%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -6,11 +6,14 @@ public:
     for(int i=0;i<nums.size();i++){
         m1[nums[i]]++;
     }
+    // copy map into vector
     vector<pair<int,int>> vec(m1.begin(),m1.end());
+    // sort the vector in descending order
     sort(vec.begin(),vec.end(),[](auto a,auto b){
         return a.second>b.second;
     });
     int count=1;
+    // store the frequent elements in another vector
     for(auto it:vec){
         if(count<=k){
             res.push_back(it.first);
