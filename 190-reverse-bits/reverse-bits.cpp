0a8fbb5794678330997e20eb36150ede8f180c21
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-10-14 09:02:07 +0530

    Time: 3 ms (29.89%) | Memory: 8.6 MB (5.55%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+    vector<int> arr;
+    while(n>0){
+        arr.push_back(n&1);
+        n=n>>1;
+    }
+    while(arr.size()<32){
+        arr.push_back(0);
+    }
+    unsigned int res=0;
+    for(int i=arr.size()-1;i>=0;i--){
+        unsigned int temp=pow(2,31-i);
+        res+=temp*arr[i];
+    }
+    return res;
+    }
+};
