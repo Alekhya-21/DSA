c9ff1e845a314cabfd956a20b50743a06ef74cf3
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-10-14 09:04:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (7.56%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -6,9 +6,9 @@ public:
         arr.push_back(n&1);
         n=n>>1;
     }
-    while(arr.size()<32){
-        arr.push_back(0);
-    }
+    // while(arr.size()<32){
+    //     arr.push_back(0);
+    // }
     unsigned int res=0;
     for(int i=arr.size()-1;i>=0;i--){
         unsigned int temp=pow(2,31-i);
