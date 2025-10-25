5a4a2b3a8673d2922a7d5f7a122c0e86ad95aad8
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-10-25 13:38:53 +0530

    Time: 7 ms (51.44%) | Memory: 13.6 MB (7.81%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int romanToInt(string s) {
-    // subtract if smaller before than bigger that is the logic
-    unordered_map<char,int> m1={{
-        'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
-    int count=0;
-    for(int i=0;i<s.length();i++){
-        if(i+1<s.length() && m1[s[i]]<m1[s[i+1]]){
-            count=count+m1[s[i+1]]-m1[s[i]];
+    unordered_map<char,int> mp={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+    int sum=0;
+    for(int i=0;i<s.size();i++){
+        if(mp[s[i]]<mp[s[i+1]]){
+            sum=sum+mp[s[i+1]]-mp[s[i]];
             i++;
         }
         else{
-            count=count+m1[s[i]];
+            sum+=mp[s[i]];
         }
     }
-    return count;
+    return sum;
     }
 };
