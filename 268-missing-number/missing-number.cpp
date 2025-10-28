0209a02c5bbced0e1abcba74dafe4c5eb741fe1d
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-10-28 21:22:12 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (37.87%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,42 +1,17 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-    // int numsSize=nums.size();
-    // for(int i=0;i<numsSize;i++){
-    //     for(int j=i+1;j<numsSize;j++){
-    //         if(nums[i]>nums[j]){
-    //             int temp=nums[i];
-    //             nums[i]=nums[j];
-    //             nums[j]=temp;
-    //         }
-    //     }
-    // }
-    // if(nums[0]!=0){
-    //     return 0;
-    // }
-    // for(int i=1;i<numsSize;i++){
-    //     if((nums[i]-nums[i-1])>1){
-    //         return(nums[i-1]+1);
-    //     }
-    // }
-    // return numsSize;
-
-    // using bitwise
-    sort(nums.begin(),nums.end());
-    if(nums[0]!=0){
-        return 0;
-    }
-    int last=nums[nums.size()-1];
-    int res=nums[0];
-    for(int i=1;i<nums.size();i++){
-        res=res^nums[i];
-    }
-    for(int i=0;i<=last;i++){
-        res=res^i;
-    }
-    if(res!=0){
+        sort(nums.begin(),nums.end());
+        int largest=nums[nums.size()-1];
+        if(nums[0]!=0)return 0;
+        int res=0;
+        for(int i=1;i<nums.size();i++){
+            res=res^nums[i];
+        }
+        for(int i=0;i<=largest;i++){
+            res=res^i;
+        }
+        if(res==0)return largest+1;
         return res;
-    }
-    return last+1;
     }
 };
