21c4697ef29281fb00dc899842b91fa74ea3fabb
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-10-29 19:38:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (85.21%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -5,5 +5,17 @@ public:
         return sqrt(x);
 
         // using binary search
+        int low=0;
+        int high=x;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(mid*mid==x)return mid;
+            else if(mid*mid>x){
+                high=low-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
     }
 };
