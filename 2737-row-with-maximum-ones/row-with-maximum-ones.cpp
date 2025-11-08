e3c8d0bc47b35f794a17672928373b1b7dc04afd
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-11-08 19:15:03 +0530

    Time: 2 ms (69.11%) | Memory: 64.7 MB (34.49%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
@@ -1,11 +1,43 @@
 class Solution {
 public:
+    int lower_bound(vector<int>& mat,int x){
+        int ans=0;
+        int low=0;
+        int high=mat.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(mat[mid]>=x){
+                ans=mat.size()-mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
     vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+        // brute force
+        // int max_count=0;
+        // int ind=0;
+        // int res_count=0;
+        // for(int i=0;i<mat.size();i++){
+        //     int cow=count(mat[i].begin(),mat[i].end(),1);
+        //         if(cow>max_count){
+        //             max_count=cow;
+        //             ind=i;
+        //             res_count=cow;
+        //         }
+        // }
+        // return {ind,res_count};
+
+        // binary search
         int max_count=0;
         int ind=0;
         int res_count=0;
         for(int i=0;i<mat.size();i++){
-            int cow=count(mat[i].begin(),mat[i].end(),1);
+            sort(mat[i].begin(),mat[i].end());
+            int cow=lower_bound(mat[i],1);
                 if(cow>max_count){
                     max_count=cow;
                     ind=i;
