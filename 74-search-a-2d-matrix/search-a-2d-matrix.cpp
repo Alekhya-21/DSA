7aed517575819717a743b859a7d7da63895cfdfa
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-11-10 12:42:38 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (75.47%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool func(vector<int>& matrix,int target,int n){
+        bool flag=false;
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(matrix[mid]==target){
+                flag=true;
+                return flag;
+            }
+            else if(target<matrix[mid]){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+            mid=(low+high)/2;
+            if((mid==0 || mid==n-1) && (matrix[mid]!=target)){
+                flag=false;
+                return flag;
+            }
+        }
+        return flag;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int low=0,high=m-1;
+        bool search=false;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(target>=matrix[mid][0] && target<=matrix[mid][n-1]){
+                search=func(matrix[mid],target,n);
+                return search;
+            }
+            else if(target<matrix[mid][0]){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return search;
+    }
+};
