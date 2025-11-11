7af45764dc23ee71a0ede2b92d2409e11a05623f
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-11-11 13:44:25 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (45.19%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,27 +1,44 @@
+// class Solution {
+// int bs(vector<int>& nums,int low,int high,int target){
+//     if(low>high){
+//         return -1;
+//     }
+//     int mid=(low+high)/2;
+//     if(nums[mid]==target) return mid;
+//     else if(target<nums[mid]) return bs(nums,low,mid-1,target);
+//     else return bs(nums,mid+1,high,target);
+// }
+// public:
+//     int search(vector<int>& nums, int target) {
+//     // iterative
+//     // int low=0;
+//     // int high=nums.size()-1;
+//     // while(low<=high){
+//     //     int mid=(low+high)/2;
+//     //     if(target==nums[mid]) return mid;
+//     //     else if(target<nums[mid]) high=mid-1;
+//     //     else low=mid+1;
+//     // }
+//     // return -1;
+
+//     // recursive
+//     return bs(nums,0,nums.size()-1,target);
+//     }
+// };
+
 class Solution {
-int bs(vector<int>& nums,int low,int high,int target){
-    if(low>high){
-        return -1;
-    }
-    int mid=(low+high)/2;
-    if(nums[mid]==target) return mid;
-    else if(target<nums[mid]) return bs(nums,low,mid-1,target);
-    else return bs(nums,mid+1,high,target);
-}
 public:
     int search(vector<int>& nums, int target) {
-    // iterative
-    // int low=0;
-    // int high=nums.size()-1;
-    // while(low<=high){
-    //     int mid=(low+high)/2;
-    //     if(target==nums[mid]) return mid;
-    //     else if(target<nums[mid]) high=mid-1;
-    //     else low=mid+1;
-    // }
-    // return -1;
-
-    // recursive
-    return bs(nums,0,nums.size()-1,target);
+    // iterative 
+    int low=0;
+    int high=nums.size()-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(nums[mid]==target)return mid;
+        else if(nums[mid]<target)low=mid+1;
+        else high=mid-1;
+    }
+    return -1;
+    // recursion
     }
 };
