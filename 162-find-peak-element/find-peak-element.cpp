ac4197de501d2a63494b969d203cf2537e954ee8
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-11-11 19:35:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (93.28%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,48 +1,27 @@
-// class Solution {
-// public:
-//     int findPeakElement(vector<int>& nums) {
-//         int low=1;
-//         int high=nums.size()-2;
-//         int n=nums.size();
-//         if(n==1){return 0;}
-//         if(nums.size()>1 && nums[0]>nums[1])return 0;
-//         if(nums.size()>1 && nums[n-1]>nums[n-2])return n-1;
-//         int res;
-//         while(low<=high){
-//             int mid=(low+high)/2;
-//             if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
-//             res=mid;
-//             }
-//             // check if it in increasing or decreasing order
-//             if(nums[mid]<nums[mid-1])high=mid-1;
-//             else low=mid+1;
-//         }
-//         return res;
-//     }
-// };
-
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1) return 0;
-        if(nums[0]>nums[1])return 0;
-        if(nums[n-1]>nums[n-2])return n-1;
         int low=1;
-        int high=n-2;
+        int high=nums.size()-2;
+        int n=nums.size();
+        if(n==1){return 0;}
+        if(nums.size()>1 && nums[0]>nums[1])return 0;
+        if(nums.size()>1 && nums[n-1]>nums[n-2])return n-1;
         int res;
         while(low<=high){
             int mid=(low+high)/2;
-            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) {
-                res=mid;
-                return res;
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+            res=mid;
             }
-            else if(nums[mid]>nums[mid-1])low=mid+1;
-            else high=mid-1;
+            // check if it in increasing or decreasing order
+            if(nums[mid]<nums[mid-1])high=mid-1;
+            else low=mid+1;
         }
         return res;
     }
 };
+
+
 
 
 
