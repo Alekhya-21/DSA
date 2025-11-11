5ac0e81d6d872d3885bef52a7de90f225253bdeb
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-11-11 21:27:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (50.45%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,21 +1,60 @@
+// class Solution {
+// public:
+//     int mySqrt(int x) {
+//         if(x==1 || x==0) return x;
+//         int low=1;
+//         int high=x;
+//         int res=0;
+//         while(low<=high){
+//             long long mid=low+(high-low)/2;
+//             if((mid*mid)==x){
+//                 return mid;
+//             }
+//             else if(mid*mid<=x){
+//                 res=mid;
+//                 low=mid+1;
+//             }
+//             else{
+//                 high=mid-1;
+//             }
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
     int mySqrt(int x) {
-        // formula
-        return sqrt(x);
-
-        // using binary search
-        int low=0;
+        int low=1;
         int high=x;
+        long res;
+        if(x==0)return 0;
         while(low<=high){
-            int mid=(low+high)/2;
-            if(mid*mid==x)return mid;
-            else if(mid*mid>x){
-                high=low-1;
+            long long mid=low+(high-low)/2;
+            if(mid*mid==x){
+                res=mid;
+                return res;
             }
-            else{
+            else if(mid*mid<=x){
+                res=mid;
                 low=mid+1;
             }
+            else{
+                high=mid-1;
+            }
         }
+        return res;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
