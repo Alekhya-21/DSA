e31a052147af633f4b980c706675256ca6b29e70
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-11-11 18:32:26 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (47.68%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,29 +1,67 @@
+// class Solution {
+// public:
+//     int findMin(vector<int>& nums) {
+//         int low=0;
+//         int high=nums.size()-1;
+//         int min=nums[low];
+//         while(low<=high){
+//             int mid=(low+high)/2;
+//             if(nums[mid]<min){
+//                 min=nums[mid];
+//             }
+//             // left sorted
+//             if(nums[low]<=nums[mid]){
+//                 if(nums[low]<min){
+//                     min=nums[low];
+//                 }
+//                 low=mid+1;
+//             }
+//             // right sorted
+//             else{
+//                 if(nums[mid]<min){
+//                     min=nums[mid];
+//                 }
+//                 high=mid-1;
+//             }
+//         }
+//         return min;
+//     }
+// };
+
 class Solution {
 public:
-    int findMin(vector<int>& nums) {
+    int findMin(vector<int>& arr) {
         int low=0;
-        int high=nums.size()-1;
-        int min=nums[low];
+        int high=arr.size()-1;
+        int res=arr[0];
         while(low<=high){
             int mid=(low+high)/2;
-            if(nums[mid]<min){
-                min=nums[mid];
-            }
             // left sorted
-            if(nums[low]<=nums[mid]){
-                if(nums[low]<min){
-                    min=nums[low];
+            if(arr[low]<=arr[mid]){
+                if(arr[low]<=res){
+                    res=arr[low];
                 }
                 low=mid+1;
             }
             // right sorted
             else{
-                if(nums[mid]<min){
-                    min=nums[mid];
+                if(arr[mid]<=res){
+                    res=arr[mid];
                 }
                 high=mid-1;
             }
         }
-        return min;
+    return res;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
