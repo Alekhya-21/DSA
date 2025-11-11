3d10870aa2a8c215188237b86a0d3268b3fe9f3f
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-11-11 17:22:49 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (89.53%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,70 +1,29 @@
-// class Solution {
-// public:
-//     bool search(vector<int>& nums, int target) {
-//         int low=0;
-//         int high=nums.size()-1;
-//         while(low<=high){
-//             int mid=(low+high)/2;
-//             if(nums[mid]==target) return 1;
-//             // trim down if nums[low]=nums[mid]=nums[high]
-//             if(nums[low]==nums[mid] && nums[mid]==nums[high]){
-//                 low++;
-//                 high--;
-//                 continue;
-//             }
-//             // left sorted
-//             if(nums[low]<=nums[mid]){
-//                 if(target<nums[mid] && target>=nums[low])high=mid-1;
-//                 else low=mid+1;
-//             }
-//             // right sorted
-//             else{
-//                 if(target<=nums[high] && target>nums[mid]) low=mid+1;
-//                 else high=mid-1;
-//             }
-//         }
-//         return 0;
-//     }
-// };
-
-
 class Solution {
 public:
-    bool search(vector<int>& arr, int target) {
+    bool search(vector<int>& nums, int target) {
         int low=0;
-        int high=arr.size()-1;
-        int flag=false;
+        int high=nums.size()-1;
         while(low<=high){
             int mid=(low+high)/2;
-            if(arr[mid]==target){
-                flag=true;
-                return flag;
-            }
-            if(arr[low]==arr[mid] && arr[high]==arr[mid]){
+            if(nums[mid]==target) return 1;
+            // trim down if nums[low]=nums[mid]=nums[high]
+            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
                 low++;
                 high--;
                 continue;
             }
             // left sorted
-            if(arr[low]<=arr[mid]){
-                if(target<=arr[mid] && target>=arr[low]){
-                    high=mid-1;
-                }
-                else{
-                    low=mid+1;
-                }
+            if(nums[low]<=nums[mid]){
+                if(target<nums[mid] && target>=nums[low])high=mid-1;
+                else low=mid+1;
             }
             // right sorted
             else{
-                if(target>=arr[mid] && target<=arr[high]){
-                    low=mid+1;
-                }
-                else{
-                    high=mid-1;
-                }
+                if(target<=nums[high] && target>nums[mid]) low=mid+1;
+                else high=mid-1;
             }
         }
-        return flag;
+        return 0;
     }
 };
 
