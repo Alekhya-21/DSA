9b62315ed5d3801b127e44d1e3d4e4e751c4a489
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-11-12 15:10:47 +0530

    Time: 0 ms (100.00%) | Memory: 50.6 MB (28.79%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -1,65 +1,27 @@
-// class Solution {
-// public:
-//     int func(vector<vector<int>>& mat,int m,int n,int mid){
-//         int row;
-//         int maxi=0;
-//         for(int i=0;i<m;i++){
-//             if(mat[i][mid]>maxi){
-//                 maxi=mat[i][mid];
-//                 row=i;
-//             }
-//         }
-//         return row;
-//     }
-//     vector<int> findPeakGrid(vector<vector<int>>& mat) {
-//         int m=mat.size();
-//         int n=mat[0].size();
-//         int low=0;
-//         int high=n-1;
-//         vector<int> res;
-//         while(low<=high){
-//             int mid=(low+high)/2;
-//             int row=func(mat,m,n,mid);
-//             int left=mid-1>0 ? mat[row][mid-1]:-1;
-//             int right=mid+1<n ? mat[row][mid+1]:-1;
-//             if(mat[row][mid]>left && mat[row][mid]>right){
-//                 res={row,mid};
-//                 return res;
-//             }
-//             else if(mat[row][mid]<left){
-//                 high=mid-1;
-//             }
-//             else{
-//                 low=mid+1;
-//             }
-//         }
-//         return res;
-//     }
-// };
-
 class Solution {
 public:
-    int func(vector<vector<int>>& mat,int mid,int m,int n){
+    int func(vector<vector<int>>& mat,int m,int n,int mid){
+        int row;
         int maxi=0;
-        int row_ind;
         for(int i=0;i<m;i++){
             if(mat[i][mid]>maxi){
                 maxi=mat[i][mid];
-                row_ind=i;
+                row=i;
             }
         }
-        return row_ind;
+        return row;
     }
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int m=mat.size();
         int n=mat[0].size();
+        int low=0;
+        int high=n-1;
         vector<int> res;
-        int low=0,high=n-1;
         while(low<=high){
             int mid=(low+high)/2;
-            int row=func(mat,mid,m,n);
-            int left=mid-1>0?mat[row][mid-1]:-1;
-            int right=mid+1<n?mat[row][mid+1]:-1;
+            int row=func(mat,m,n,mid);
+            int left=mid-1>0 ? mat[row][mid-1]:-1;
+            int right=mid+1<n ? mat[row][mid+1]:-1;
             if(mat[row][mid]>left && mat[row][mid]>right){
                 res={row,mid};
                 return res;
@@ -74,3 +36,4 @@ public:
         return res;
     }
 };
+
