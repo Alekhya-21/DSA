485208afdc47cc4170f062c32e1b02a1cff8b54b
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-11-26 14:02:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (31.13%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,49 +1,23 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-    int low=0;
-    int high=numbers.size()-1;
-    while(low<high){
-        int sum=numbers[low]+numbers[high];
-        if(sum==target){
-            return {low+1,high+1};
+        int low=0;
+        int high=numbers.size()-1;
+        int left,right;
+        while(low<high){
+            int sum=numbers[low]+numbers[high];
+            if(sum==target){
+                left=low+1;
+                right=high+1;
+                return {left,right};
+            }
+            else if(sum>target){
+                high--;
+            }
+            else{
+                low++;
+            }
         }
-        else if(sum>target){
-            high--;
-        }
-        else{
-            low++;
-        }
-    }
-    return {};
-// way 2
-
-    // int low=0,high=numbers.size()-1;
-    // int num1=0,num2=0;
-    // while(low<high){
-    //     int sum=numbers[low]+numbers[high];
-    //     if(sum==target){
-    //         num1=numbers[low];
-    //         num2=numbers[high];
-    //         break;
-    //     }
-    //     else if(sum>target){
-    //         high--;
-    //     }
-    //     else{
-    //         low++;
-    //     }
-    // }
-    // int left=-1,right=-1;
-    // for(int i=0;i<numbers.size();i++){
-    //     if(numbers[i]==num1 && left==-1){
-    //         left=i+1;
-    //         i++;
-    //     }
-    //     if(numbers[i]==num2 && right==-1){
-    //         right=i+1;
-    //     }
-    // }
-    // return {left,right};
+        return {left,right};
     }
 };
