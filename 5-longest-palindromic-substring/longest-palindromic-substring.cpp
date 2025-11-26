89152a8f8dc03bbfa11da63ba984fc3fa90cd61a
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-11-26 19:21:48 +0530

    Time: 15 ms (56.62%) | Memory: 18.6 MB (42.50%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        if(s.length()<=0){
-            return s;
-        }
-        // creating an empty string which stores the result string
         string res="";
-        // we are applying Expand around center logic
-        // consider odd length
-        for(int i=0;i<s.length();i++){
+        for(int i=0;i<s.size();i++){
             int low=i;
             int high=i;
-            // expanding to the left and right from center
-            while(low>=0 && high<s.length() && s[low]==s[high]){
+            while(low>=0 && high<s.size() && s[low]==s[high]){
                 low--;
                 high++;
             }
-            // try to replace the longest palindrome substring
-            string palindrome=s.substr(low+1,high-low-1);
-            if(palindrome.length()>res.length()){
-                res=palindrome;
+            string temp=s.substr(low+1,high-low-1);
+            if(temp.size()>res.size()){
+                res=temp;
             }
-            // consider even length
-            low=i-1;
-            high=i;
-            while(low>=0 && high<s.length() && s[low]==s[high]){
+            low=i;
+            high=i+1;
+            while(low>=0 && high<s.size() && s[low]==s[high]){
                 low--;
                 high++;
             }
-            palindrome=s.substr(low+1,high-low-1);
-            if(palindrome.length()>res.length()){
-                res=palindrome;
+            temp=s.substr(low+1,high-low-1);
+            if(temp.size()>res.size()){
+                res=temp;
             }
         }
         return res;
