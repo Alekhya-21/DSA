06e5809179c5d1fb378522093dea36c1af282c2f
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-11-27 12:14:10 +0530

    Time: 736 ms (5.01%) | Memory: 267.8 MB (5.04%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,51 @@
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//     string res="";
+//     for(int i=0;i<s.length();i++){
+//         int low=i;
+//         int high=i;
+//         set<char> visited;
+//         while(high<s.length() &&
+//         visited.find(s[high])==visited.end()){
+//             visited.insert(s[high]);
+//             high++;
+//         }
+//         string temp=s.substr(low,high-low);
+//         if(temp.length()>res.length()){
+//             res=temp;
+//         }
+//     }
+//     return res.length();
+//     }
+// };
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-    string res="";
-    for(int i=0;i<s.length();i++){
-        int low=i;
-        int high=i;
+    int max_len=0;
+    for(int i=0;i<s.size();i++){
         set<char> visited;
-        while(high<s.length() &&
-        visited.find(s[high])==visited.end()){
-            visited.insert(s[high]);
-            high++;
+        int j=i;
+        while(j<s.size() && visited.find(s[j])==visited.end()){
+            visited.insert(s[j]);
+            j++;
         }
-        string temp=s.substr(low,high-low);
-        if(temp.length()>res.length()){
-            res=temp;
+        if(visited.size()>max_len){
+            max_len=visited.size();
         }
     }
-    return res.length();
+    return max_len;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
