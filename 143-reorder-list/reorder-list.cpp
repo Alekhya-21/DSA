dd605aa5e078aaa30ea208844c98c4412a2aafce
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2025-12-03 14:38:24 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (23.76%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -1,3 +1,47 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// ListNode* reverse(ListNode* head){
+//     if(head==NULL || head->next==NULL){
+//         return head;
+//     }
+//     ListNode* newhead=reverse(head->next);
+//     ListNode* front=head->next;
+//     front->next=head;
+//     head->next=NULL;
+//     return newhead;
+// }
+// public:
+//     void reorderList(ListNode* head) {
+//         ListNode* slow=head;
+//         ListNode* fast=head;
+//         while(fast->next!=NULL && fast->next->next!=NULL){
+//             slow=slow->next;
+//             fast=fast->next->next;
+//         }
+//         ListNode* newhead=reverse(slow->next);
+//         slow->next=NULL;
+//         ListNode* first=head;
+//         ListNode* second=newhead;
+//         while(first!=NULL && second!=NULL){
+//             ListNode* t1=first->next;
+//             ListNode* t2=second->next;
+//             first->next=second;
+//             second->next=t1;
+//             first=t1;
+//             second=t2;
+//         }
+//     }
+// };
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -31,13 +75,24 @@ public:
         slow->next=NULL;
         ListNode* first=head;
         ListNode* second=newhead;
-        while(first!=NULL && second!=NULL){
-            ListNode* t1=first->next;
-            ListNode* t2=second->next;
+        while(second!=NULL){
+            ListNode* temp1=first->next;
+            ListNode* temp2=second->next;
             first->next=second;
-            second->next=t1;
-            first=t1;
-            second=t2;
+            second->next=temp1;
+            first=temp1;
+            second=temp2;
         }
     }
 };
+
+
+
+
+
+
+
+
+
+
+
