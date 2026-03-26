42549252afb522ee816dbc6b6d5d358017944323
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2026-03-26 11:42:49 +0530

    Time: 1359 ms (5.03%) | Memory: 291.7 MB (7.70%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -3,22 +3,22 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         set<vector<int>> arr;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<=(nums.size()-3);i++){
-            int low=i+1;
-            int high=nums.size()-1;
-            while(low<high){
-                int sum=nums[i]+nums[low]+nums[high];
-                if(sum==0){
-                    arr.insert({nums[i],nums[low],nums[high]});
-                    low++;
-                    high--;
-                }
-                else if(sum>0){
-                    high--;
-                }
-                else{
-                    low++;
-                }
+        for(int i=0;i<nums.size()-2;i++){
+            int j=i+1;
+            int k=nums.size()-1;
+            while(j<k){
+            int sum=nums[i]+nums[j]+nums[k];
+            if(sum==0){
+                arr.insert({nums[i],nums[j],nums[k]});
+                j++;
+                k--;
+            }
+            else if(sum>0){
+                k--;
+            }
+            else{
+                j++;
+            }
             }
         }
         return vector<vector<int>>(arr.begin(),arr.end());
