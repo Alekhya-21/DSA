b5f89fd0ee3744971cbaabfd1ffa6a71acd21d20
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2026-03-30 17:01:12 +0530

    Time: 13 ms (20.10%) | Memory: 11 MB (39.68%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int low=0,high=0;
-        int max_freq=0;
+        int curr_window=0;
         int window_size=0;
-        int max_window=0;
-        unordered_map<char,int> count;
+        int max_window=INT_MIN;
+        int max_frequency=INT_MIN;
+        map<int,int> mp;
+        int high=0;
+        int low=0;
         while(high<s.size()){
-            count[s[high]]++;
-            window_size=high-low+1;
-            max_freq=max(max_freq,count[s[high]]);
-            if((window_size-max_freq)>k){
-                count[s[low]]--;
+            mp[s[high]]++;
+            max_frequency=max(max_frequency,mp[s[high]]);
+            curr_window=high-low+1;
+            while(curr_window-max_frequency>k){
+                mp[s[low]]--;
                 low++;
+                curr_window=high-low+1;
             }
-            max_window=max(max_window,high-low+1);
+            max_window=max(max_window,curr_window);
             high++;
         }
         return max_window;
