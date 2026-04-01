5d957ed1f4253c120904a3c372ddfaef0e1cf458
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2026-04-01 17:08:03 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (53.36%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,23 +1,49 @@
+// class Solution {
+// public:
+//     void func(int ind,int target,vector<int>& candidates,vector<int>& temp,vector<vector<int>>& res){
+//         if(ind==candidates.size()){
+//             if(target==0){
+//                 res.push_back(temp);
+//             }
+//             return;
+//         }
+//         if(candidates[ind]<=target){
+//             temp.push_back(candidates[ind]);
+//             func(ind,target-candidates[ind],candidates,temp,res);
+//             temp.pop_back();
+//         }
+//         func(ind+1,target,candidates,temp,res);
+//     }
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//         vector<int> temp;
+//         vector<vector<int>> res;
+//         func(0,target,candidates,temp,res);
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
-    void func(int ind,int target,vector<int>& candidates,vector<int>& temp,vector<vector<int>>& res){
-        if(ind==candidates.size()){
+    void solve(int i,vector<int>& arr, int target,vector<int>& candidates,vector<vector<int>>& res){
+        if(i==candidates.size()){
             if(target==0){
-                res.push_back(temp);
+                res.push_back(arr);
             }
             return;
         }
-        if(candidates[ind]<=target){
-            temp.push_back(candidates[ind]);
-            func(ind,target-candidates[ind],candidates,temp,res);
-            temp.pop_back();
+        if(candidates[i]<=target){
+            arr.push_back(candidates[i]);
+            target-=candidates[i];
+            solve(i,arr,target,candidates,res);
+            target+=candidates[i];
+            arr.pop_back();
         }
-        func(ind+1,target,candidates,temp,res);
+        solve(i+1,arr,target,candidates,res);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> temp;
+        vector<int> arr;
         vector<vector<int>> res;
-        func(0,target,candidates,temp,res);
+        solve(0,arr,target,candidates,res);
         return res;
     }
 };
