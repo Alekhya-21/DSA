8b39d486a720d20a3319d2c9b5d68e503a7a1add
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2026-04-07 14:24:14 +0530

    Time: 14 ms (23.13%) | Memory: 7.7 MB (64.54%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,17 +1,27 @@
-// Iterative
+// // Iterative
+// class Solution {
+// public:
+//     int fib(int n) {
+//         int a=0;
+//         int b=1;
+//         int c;
+//         if(n==0) return 0;
+//         if(n==1) return 1;
+//         for(int i=2;i<=n;i++){
+//             c=a+b;
+//             a=b;
+//             b=c;
+//         }
+//         return c;
+//     }
+// };
+
+// Recursive
 class Solution {
 public:
     int fib(int n) {
-        int a=0;
-        int b=1;
-        int c;
         if(n==0) return 0;
         if(n==1) return 1;
-        for(int i=2;i<=n;i++){
-            c=a+b;
-            a=b;
-            b=c;
-        }
-        return c;
+        return fib(n-1)+fib(n-2);
     }
 };
