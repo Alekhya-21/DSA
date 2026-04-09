b6979db77bb08ffeda5212539126c51b709658f2
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2026-04-09 13:42:00 +0530

    Time: 19 ms (55.77%) | Memory: 12.3 MB (92.25%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,15 +1,37 @@
+# # Iterative
+# class Solution(object):
+#     def fib(self, n):
+#         a=0
+#         b=1
+#         c=0
+#         if(n==0):
+#             return 0
+#         if(n==1):
+#             return 1
+#         for i in range(2,n+1):
+#             c=a+b
+#             a=b
+#             b=c
+#         return c
+
+# Recursive
+# class Solution(object):
+#     def fib(self, n):
+#         if(n==0):
+#             return 0
+#         if(n==1):
+#             return 1
+#         return self.fib(n-1)+self.fib(n-2)
+    
+# DP
 class Solution(object):
+    def func(self,n,dp):
+        if(n<=1):
+            return n
+        if(dp[n]!=-1):
+            return dp[n]
+        dp[n]=self.func(n-1,dp)+self.func(n-2,dp)
+        return dp[n]
     def fib(self, n):
-        a=0
-        b=1
-        c=0
-        if(n==0):
-            return 0
-        if(n==1):
-            return 1
-        for i in range(2,n+1):
-            c=a+b
-            a=b
-            b=c
-        return c
-        
+        dp=[-1]*(n+1)
+        return self.func(n,dp)
