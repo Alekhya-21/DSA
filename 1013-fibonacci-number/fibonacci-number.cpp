2af08f18cfc2bf46e17313271f0c7169d4efc83d
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2026-04-09 12:48:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (10.70%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,4 +1,5 @@
 // // Iterative
+// TC- O(n), SC- O(1)
 // class Solution {
 // public:
 //     int fib(int n) {
@@ -16,12 +17,29 @@
 //     }
 // };
 
+
 // Recursive
+// TC- O(2^n), SC- O(n)
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if(n==0) return 0;
+//         if(n==1) return 1;
+//         return fib(n-1)+fib(n-2);
+//     }
+// };
+
+// DP
 class Solution {
 public:
+    int func(int n,vector<int>& dp){
+        if(n<=1) return n;
+        if(dp[n]!=-1) return dp[n];
+        dp[n]=func(n-1,dp)+func(n-2,dp);
+        return dp[n];
+    }
     int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return fib(n-1)+fib(n-2);
+        vector<int> dp(n+1,-1);
+        return func(n,dp);
     }
 };
