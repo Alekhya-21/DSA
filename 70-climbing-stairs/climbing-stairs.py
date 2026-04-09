85c77485fee8477163d75a5d98d6e45553407d51
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2026-04-09 14:35:08 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (98.59%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def func(self,n,dp):
+        if(n<=1):
+            return 1
+        if(dp[n]!=-1):
+            return dp[n]
+        dp[n]=self.func(n-1,dp)+self.func(n-2,dp)
+        return dp[n]
+    def climbStairs(self, n: int) -> int:
+        dp=[-1]*(n+1)
+        return self.func(n,dp)
