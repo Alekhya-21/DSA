72dafa0aa1a13f00dd27c76b75967dc7af659897
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2026-04-09 13:58:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (10.70%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -29,18 +29,36 @@
 //     }
 // };
 
-// DP
+// DP Recursive
+//TC- O(n), SC- O(n)
+// class Solution {
+// public:
+//     int func(int n,vector<int>& dp){
+//         if(n<=1) return n;
+//         if(dp[n]!=-1) return dp[n];
+//         dp[n]=func(n-1,dp)+func(n-2,dp);
+//         return dp[n];
+//     }
+//     int fib(int n) {
+//         vector<int> dp(n+1,-1);
+//         return func(n,dp);
+//     }
+// };
+
+// DP Iterative
 //TC- O(n), SC- O(n)
 class Solution {
 public:
-    int func(int n,vector<int>& dp){
-        if(n<=1) return n;
-        if(dp[n]!=-1) return dp[n];
-        dp[n]=func(n-1,dp)+func(n-2,dp);
-        return dp[n];
-    }
     int fib(int n) {
         vector<int> dp(n+1,-1);
-        return func(n,dp);
+        if(n<=1){
+            return n;
+        }
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
