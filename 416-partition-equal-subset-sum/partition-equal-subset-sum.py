226922ee3e6af98e3c1653fbefca14fa87df07fa
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2026-04-16 18:36:48 +0530

    Time: 919 ms (41.48%) | Memory: 145 MB (19.65%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,45 +1,43 @@
 # Recursion- time limit error
 # class Solution:
-#     def func(self,i,target,nums,sum):
-#         if(target==sum//2):
+#     def func(self,i,target,nums,summ):
+#         if(target==summ//2):
 #             return True
-#         if(i==len(nums) or target>(sum//2)):
+#         if(i==len(nums) or target>(summ//2)):
 #             return False
-#         if self.func(i+1,target+nums[i],nums,sum):
+#         if self.func(i+1,target+nums[i],nums,summ):
 #             return True
-#         if self.func(i+1,target,nums,sum):
+#         if self.func(i+1,target,nums,summ):
 #             return True
 #         return False
 #     def canPartition(self, nums: List[int]) -> bool:
-#         sum=0
+#         summ=0
 #         for i in nums:
-#             sum+=i
-#         if(sum%2!=0): 
+#             summ+=i
+#         if(summ%2!=0): 
 #             return False
-#         return self.func(0,0,nums,sum)
+#         return self.func(0,0,nums,summ)
 
 # DP- recursion
 class Solution:
-    def func(self,i,target,nums,sum,dp):
-        if(target==sum//2):
+    def func(self,i,target,nums,summ,dp):
+        if(target==summ//2):
             return True
-        if(i==len(nums) or target>(sum//2)):
+        if(i==len(nums) or target>(summ//2)):
             return False
         if (i,target) in dp:
             return dp[(i,target)]
-        pick=self.func(i+1,target+nums[i],nums,sum,dp)
+        pick=self.func(i+1,target+nums[i],nums,summ,dp)
         if pick:
             return True
-        dont_pick=self.func(i+1,target,nums,sum,dp)
+        dont_pick=self.func(i+1,target,nums,summ,dp)
         if dont_pick:
             return True
         dp[(i,target)]=pick or dont_pick
         return dp[(i,target)]
     def canPartition(self, nums: List[int]) -> bool:
-        sum=0
-        for i in nums:
-            sum+=i
-        if(sum%2!=0): 
+        summ = sum(nums)
+        if(summ%2!=0): 
             return False
         dp={}
-        return self.func(0,0,nums,sum,dp)
+        return self.func(0,0,nums,summ,dp)
