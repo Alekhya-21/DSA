79ecd56b0911cff2ea3590962f74d6975b19be55
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2026-04-23 15:44:29 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (46.86%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,16 +1,3 @@
-#Recursion- error time limit exceeded
-# class Solution:
-#     def func(self,i,j):
-    #     if(i==0 and j==0):
-    #         return 1
-    #     if(i<0 or j<0):
-    #         return 0
-    #     up=self.func(i-1,j)
-    #     left=self.func(i,j-1)
-    #     return up+left
-    # def uniquePaths(self, m: int, n: int) -> int:
-    #     return self.func(m-1,n-1)
-
 class Solution:
     def func(self,i,j,dp):
         if(i==0 and j==0):
@@ -19,10 +6,10 @@ class Solution:
             return 0
         if(dp[i][j]!=-1):
             return dp[i][j]
-        up=self.func(i-1,j,dp)
         left=self.func(i,j-1,dp)
-        dp[i][j]=up+left
+        up=self.func(i-1,j,dp)
+        dp[i][j]=left+up
         return dp[i][j]
     def uniquePaths(self, m: int, n: int) -> int:
-        dp=[[-1 for i in range(n)] for i in range(m)]
+        dp=[[-1 for _ in range(n)] for _ in range(m)]
         return self.func(m-1,n-1,dp)
