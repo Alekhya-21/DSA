af1994064c3acbcf093f7d85bc47a9314e7ac2c8
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2026-05-15 19:53:54 +0530

    Time: 176 ms (5.07%) | Memory: 33.5 MB (47.36%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -23,19 +23,35 @@
 #             if(nums[i]==nums[i-1]):
 #                 return nums[i]
 
+# Index sort/ cyclic sort TC-O(n),SC-O(1)
+# class Solution:
+#     def findDuplicate(self, nums: List[int]) -> int:
+#         i=0
+#         while(i<len(nums)):
+#             correct=nums[i]-1
+#             if(nums[i]!=nums[correct]):
+#                 nums[i], nums[correct] = nums[correct], nums[i]
+#             else:
+#                 if(i!=correct):
+#                     return nums[i]
+#                 i+=1
+
+# Binary Search- TC-O(n log n), SC- O(1)
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        i=0
-        while(i<len(nums)):
-            correct=nums[i]-1
-            if(nums[i]!=nums[correct]):
-                nums[i], nums[correct] = nums[correct], nums[i]
+        low=0
+        high=len(nums)-1
+        while(low<high):
+            mid=(low+high)//2
+            count=0
+            for i in nums:
+                if i<=mid:
+                    count+=1
+            if(count>mid):
+                high=mid
             else:
-                if(i!=correct):
-                    return nums[i]
-                i+=1
-
-
+                low=mid+1
+        return low
 
 
 
