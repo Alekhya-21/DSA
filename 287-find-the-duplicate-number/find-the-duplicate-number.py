5aee9b91fdb4357ed2cb3684b22eb0788184e95f
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2026-05-15 15:55:34 +0530

    Time: 91 ms (9.08%) | Memory: 33.3 MB (91.52%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -6,11 +6,18 @@
 #                 if(nums[i]==nums[j]):
 #                     return nums[i]
 
-# set
+# set, TC- O(n), SC- O(n)
+# class Solution:
+#     def findDuplicate(self, nums: List[int]) -> int:
+#         my_set=set()
+#         for i in nums:
+#             if i in my_set:
+#                 return i
+#             my_set.add(i)
+
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        my_set=set()
-        for i in nums:
-            if i in my_set:
-                return i
-            my_set.add(i)
+        nums.sort()
+        for i in range(1,len(nums)):
+            if(nums[i]==nums[i-1]):
+                return nums[i]
