76bac1965ce730341b891128285b9044a8b0362b
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2026-07-06 23:51:40 +0530

    Time: 4 ms (64.88%) | Memory: 20.3 MB (78.90%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,15 +1,14 @@
-from collections import defaultdict,deque
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph=defaultdict(list)
         for u,v in prerequisites:
             graph[u].append(v)
         indegree=[0]*numCourses
-        for i in range(numCourses):
+        queue=deque([])
+        for i in range(0,numCourses):
             for neighbor in graph[i]:
                 indegree[neighbor]+=1
-        queue=deque([])
-        for i in range(numCourses):
+        for i in range(0,numCourses):
             if(indegree[i]==0):
                 queue.append(i)
         res=[]
