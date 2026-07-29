319823ae412973ae91eb97be3d6250a812cb32de
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2026-07-29 21:32:02 +0530

    Time: 7 ms (16.43%) | Memory: 19.4 MB (40.90%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,36 +1,33 @@
-from collections import deque
+from collections import defaultdict,deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        queue=deque([])
         m=len(grid)
         n=len(grid[0])
-        count_fresh=0
+        queue=deque()
+        visited=[[0]*n for _ in range(m)]
+        fresh_count=0
         for i in range(m):
             for j in range(n):
-                if(grid[i][j]==2):
+                if grid[i][j]==2:
                     queue.append((i,j,0))
-                elif(grid[i][j]==1):
-                    count_fresh+=1
-        delrow=[-1,0,1,0]
-        delcol=[0,1,0,-1]
-        visited=[[0]*n for _ in range(m)]
+                    visited[i][j]=1
+                elif grid[i][j]==1:
+                    fresh_count+=1
         maxi=0
+        delrow=[0,-1,0,1]
+        delcol=[-1,0,1,0]
         count=0
         while queue:
-            sr,sc,time=queue.popleft()
-            visited[sr][sc]=1
+            row,col,time=queue.popleft()
             maxi=max(maxi,time)
-            for i in range(0,4):
-                nrow=sr+delrow[i]
-                ncol=sc+delcol[i]
-                if(nrow>=0 and nrow<m and ncol>=0 and ncol<n and grid[nrow][ncol]==1 and visited[nrow][ncol]==0):
+            for i in range(4):
+                nrow=row+delrow[i]
+                ncol=col+delcol[i]
+                if(nrow>=0 and nrow<m and ncol>=0 and ncol<n and visited[nrow][ncol]==0 and grid[nrow][ncol]==1):
                     visited[nrow][ncol]=1
+                    grid[nrow][ncol]=2
                     queue.append((nrow,ncol,time+1))
                     count+=1
-        if(count!=count_fresh):
+        if(count!=fresh_count):
             return -1
         return maxi
-
-
-
-
