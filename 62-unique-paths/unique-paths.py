b780b813e5e7e18e6325894cf29d1e39882f37e9
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2026-08-12 20:28:28 +0530

    Time: 3 ms (19.10%) | Memory: 19.3 MB (41.92%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,34 +1,16 @@
-# class Solution:
-#     def func(self,i,j,dp):
-#         if(i==0 and j==0):
-#             return 1
-#         if(i<0 or j<0):
-#             return 0
-#         if(dp[i][j]!=-1):
-#             return dp[i][j]
-#         left=self.func(i,j-1,dp)
-#         up=self.func(i-1,j,dp)
-#         dp[i][j]=left+up
-#         return dp[i][j]
-#     def uniquePaths(self, m: int, n: int) -> int:
-#         dp=[[-1 for _ in range(n)] for _ in range(m)]
-#         return self.func(m-1,n-1,dp)
-
 class Solution:
-    def func(self,m,n,dp):
-        for i in range(0,m):
-            for j in range(0,n):
-                if(i==0 and j==0):
-                    dp[0][0]=1
-                else:
-                    left=0
-                    up=0
-                    if(i>0):
-                        up=dp[i-1][j]
-                    if(j>0):
-                        left=dp[i][j-1]
-                    dp[i][j]=left+up
-        return dp[m-1][n-1]
+    def solve(self,i,j,dp):
+        if(dp[i][j]!=-1):
+            return dp[i][j]
+        if(i==0 and j==0):
+            dp[i][j]=1
+            return dp[i][j]
+        if(i<0 or j<0):
+            return 0
+        left=self.solve(i,j-1,dp)
+        up=self.solve(i-1,j,dp)
+        dp[i][j]=left+up
+        return dp[i][j]
     def uniquePaths(self, m: int, n: int) -> int:
-        dp=[[0 for _ in range(n)] for _ in range(m)]
-        return self.func(m,n,dp)
+        dp=[[-1 for _ in range(n)] for _ in range(m)]
+        return self.solve(m-1,n-1,dp)
