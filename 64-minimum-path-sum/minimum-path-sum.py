93150d9e923e6a0d3b32d76a0d9959aa8b65e92a
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2026-08-13 19:00:09 +0530

    Time: 27 ms (21.11%) | Memory: 21.9 MB (20.04%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,17 +1,18 @@
 class Solution:
-    def func(self,i,j,dp,grid):
-        if(i==0 and j==0):
-            return grid[i][j]
-        if(i<0 or j<0):
-            return float('inf')
+    def solve(self,i,j,dp,grid):
         if(dp[i][j]!=-1):
             return dp[i][j]
-        left=grid[i][j]+self.func(i,j-1,dp,grid)
-        up=grid[i][j]+self.func(i-1,j,dp,grid)
-        dp[i][j]=min(left,up)
+        if(i==0 and j==0):
+            dp[i][j]=grid[i][j]
+            return dp[i][j]
+        if i<0 or j<0:
+            return float("inf")
+        left=grid[i][j]+self.solve(i,j-1,dp,grid)
+        right=grid[i][j]+self.solve(i-1,j,dp,grid)
+        dp[i][j]=min(left,right)
         return dp[i][j]
     def minPathSum(self, grid: List[List[int]]) -> int:
         m=len(grid)
         n=len(grid[0])
-        dp=[[-1 for _ in range(n)] for _ in range(m)]
-        return self.func(m-1,n-1,dp,grid)
+        dp=[[-1 for _ in range(n)]for _ in range(m)]
+        return self.solve(m-1,n-1,dp,grid)
