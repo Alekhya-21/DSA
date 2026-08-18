ee120439350f729d7a4f1a0e8928e6556929fb7b
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2026-08-18 13:11:27 +0530

    Time: 2825 ms (6.02%) | Memory: 20.2 MB (39.83%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -1,22 +1,23 @@
 class Solution:
-    def func(self,i,j,n,dp,matrix):
-        for j in range(0,n):
-            dp[0][j]=matrix[0][j]
-        for i in range(1,n):
-            for j in range(0,n):
-                up=matrix[i][j]+dp[i-1][j]
-                if(j>0):
-                    left_diagonal=matrix[i][j]+dp[i-1][j-1]
-                else:
-                    left_diagonal=float("inf")
-                if(j<n-1):
-                    right_diagonal=matrix[i][j]+dp[i-1][j+1]
-                else:
-                    right_diagonal=float("inf")
-                dp[i][j]=min(up,left_diagonal,right_diagonal)
-        return dp[n-1][j]
+    def solve(self,i,j,dp,matrix,m,n):
+        if(j<0 or j>=n):
+            return float("inf")
+        if(dp[i][j]!=None):
+            return dp[i][j]
+        if(i==m-1):
+            dp[i][j]=matrix[i][j]
+            return dp[i][j]
+        left=matrix[i][j]+self.solve(i+1,j-1,dp,matrix,m,n)
+        down=matrix[i][j]+self.solve(i+1,j,dp,matrix,m,n)
+        right=matrix[i][j]+self.solve(i+1,j+1,dp,matrix,m,n)
+        dp[i][j]=min(left,down,right)
+        return dp[i][j]
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        n=len(matrix)
-        dp=[[0 for _ in range(n)] for _ in range(n)]
-        self.func(n-1,n-1,n,dp,matrix)
-        return min(dp[n-1])
+        m=len(matrix)
+        n=len(matrix[0])
+        res=float("inf")
+        for i in range(n):
+            dp=[[None for _ in range(n)] for _ in range(m)]
+            ans=self.solve(0,i,dp,matrix,m,n)
+            res=min(res,ans)
+        return res
