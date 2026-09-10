5307753bc80731f8e7c8b0ea5b4bb53781e2d2e5
Alekhya <115908261+Alekhya-21@users.noreply.github.com>
2026-09-10 18:25:13 +0530

    Time: 3 ms (89.91%) | Memory: 20.3 MB (66.45%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,12 +1,12 @@
-import heapq
-from collections import defaultdict,deque
+from collections import defaultdict
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         graph=defaultdict(list)
         for u,v,w in flights:
             graph[u].append((v,w))
-        dist=[float("inf")]*n
         queue=deque()
+        dist=[float("inf")]*n
+        dist[src]=0
         queue.append((0,src))
         k+=1
         while queue and k:
@@ -14,12 +14,11 @@ class Solution:
             for i in range(size):
                 d,node=queue.popleft()
                 for neighbor,weight in graph[node]:
-                    temp_dist=d+weight
-                    if(temp_dist<dist[neighbor]):
-                            dist[neighbor]=temp_dist
-                            queue.append((temp_dist,neighbor))
+                    newd=d+weight
+                    if newd<dist[neighbor]:
+                        dist[neighbor]=newd
+                        queue.append((newd,neighbor))
             k-=1
-        if(dist[dst]==float("inf")):
+        if dist[dst]==float("inf"):
             return -1
         return dist[dst]
-
